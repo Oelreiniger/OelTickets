da45a254e662a2eff2046e2d50b5c23e3c82ab53
Unko <bigfly376@gmail.com>
2026-01-25 23:20:24 +0100

    Adds a test page for login and navigation testing

---
+++ OelTickets/Pages/Login/LoginPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace OelTickets.Pages
+{
+    /// <summary>
+    /// Interaktionslogik für LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GoSettings_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService?.Navigate(new SettingsPage());
+        }
+    }
+}
---
+++ OelTickets/Pages/Settings/SettingsPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace OelTickets.Pages
+{
+    /// <summary>
+    /// Interaktionslogik für SettingsPage.xaml
+    /// </summary>
+    public partial class SettingsPage : Page
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService?.Navigate(new LoginPage());
+        }
+    }
+}
